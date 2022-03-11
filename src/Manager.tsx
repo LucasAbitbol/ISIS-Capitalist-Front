54df9f29a1c5667749b99f8243ae3dfab4b107bb
Wassim_Aityoucef <58562600+Wasslamenace@users.noreply.github.com>
2022-03-11 15:47:33 +0100

    ptit chgmt sur manager

--- src/Manager.tsx
+++ src/Manager.tsx
@@ -28,9 +28,9 @@ services.server + manager.logo} />
  </div>
  </div> 
  <div className="infosmanager">
- <div className="managername"> {manager.name} </div>
- <div className="managercible"> {world.products.product[manager.idcible-1].name} </div>
- <div className="managercost"> { manager.seuil} </div>
+ <div className="managername"> Nom complet : {manager.name} </div>
+ <div className="managercible"> Spécialité : Il recrute automatiquement les {world.products.product[manager.idcible-1].name} </div>
+ <div className="managercost"> Prix de recrutement : { manager.seuil} $</div>
  </div>
  <div /*onClick={() => manager.hireManager(manager)}*/>
  <button disabled={world.money < manager.seuil}> Hire !</button>
